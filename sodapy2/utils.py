1c71ee79e2dab28227f52aed9b77ce848667f033
digital-nirvana <160486306+digital-nirvana@users.noreply.github.com>
2024-10-29 21:01:48 -0300

    14 make better responses (#16)
    * Add custom SodapyResponse and SodapyHttpError classes to return richer error messages.

--- sodapy2/utils.py
+++ sodapy2/utils.py
@@ -25,28 +25,3 @@ def download_file(url: str, local_fpath: str) -> None:
         for chunk in response.iter_content(chunk_size=1024):
             if chunk:  # filter out keep-alive new chunks.
                 outfile.write(chunk)
-
-
-def raise_for_status(response: requests.Response) -> None:
-    """
-    Custom raise_for_status with more appropriate error message.
-
-    Args:
-        response: a response object.
-    """
-    http_error_msg = ""
-
-    if 400 <= response.status_code < 500:
-        http_error_msg = f"{response.status_code} Client Error: {response.reason}"
-
-    elif 500 <= response.status_code < 600:
-        http_error_msg = f"{response.status_code} Server Error: {response.reason}"
-
-    if http_error_msg:
-        try:
-            more_info = response.json().get("message")
-        except ValueError:  # This will raise if there is no JSON response.
-            more_info = None
-        if more_info and more_info.lower() != response.reason.lower():
-            http_error_msg += f".\n\t{more_info}"
-        raise requests.exceptions.HTTPError(http_error_msg, response=response)
